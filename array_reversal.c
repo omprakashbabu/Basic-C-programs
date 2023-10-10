7b553d98bae5b86587f5d6bb05c831d79f207df1
Hari Aravindh <127916422+HariAr2@users.noreply.github.com>
2023-10-10 21:41:03 +0530

    Create array_reversal.c

---
+++ array_reversal.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+    for(i = num-1; i>=0;--i)//using for loop to reverse the array
+        printf("%d ", *(arr + i));
+    return 0;
+}
+/* OUTPUT
+5
+1 3 4 5 6
+6 5 4 3 1
+*/

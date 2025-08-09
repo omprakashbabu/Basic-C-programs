bec3193f3b0a2637f18ff43a439c72530a6fe1ef
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2025-08-09 23:47:35 +0530

    Update Addition of two numbers.c

--- Addition of two numbers.c
+++ Addition of two numbers.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
     void main(){  
         int a,b,sum;
-        printf("Enter two numbers : ");
+        printf("Enter two numbers : ");  
         scanf("%d %d",&a,&b);
         sum=a+b;
         printf("The sum is %d",sum);
